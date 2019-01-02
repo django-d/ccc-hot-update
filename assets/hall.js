8bb6a1057fd71d594eab179ef8dd5ff05aa4dcd9
Django <django_d@163.com>
2019-01-02 16:38:14 +0800

    跟新hall

--- assets/hall.js
+++ assets/hall.js
@@ -375,6 +375,10 @@ cc.Class({
     on_create_game: function () {
         cc.loader.loadRes('prefab/game1', cc.Prefab, (err, res) => {
             console.log('game1: =>', res);
+            const node = cc.instantiate(res);
+            node.parent = this.node;
+            node.x = 0;
+            node.y = 0;
         });
     },
 
